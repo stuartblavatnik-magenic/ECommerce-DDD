7d80353b81358d596f886f5565311aebf1cafbfa
Stuart Blavatnik <stuartb@magenic.com>
2021-06-26 19:57:21 -0400

    Initial class

---
+++ Code/src/com/twoforboth/ecommerce/model/User.java
@@ -0,0 +1,10 @@
+package com.twoforboth.ecommerce.model;
+
+import java.util.Date;
+
+public class User {
+    private Long id;
+    private String name;
+    private Boolean isEnabled;
+    private Date created;
+}
